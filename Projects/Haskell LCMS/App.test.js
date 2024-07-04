3cd4d6511ef4eecbe1bcc6f2694acc1fdc6b1e51
KIYINI Joseph Balamazze <73903994+josephkb87@users.noreply.github.com>
2024-07-04 05:20:16 +0300

    Delete Projects/Haskell LCMS directory
    Signed-off-by: KIYINI Joseph Balamazze <73903994+josephkb87@users.noreply.github.com>

--- Projects/Haskell LCMS/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders login button', () => {
-  render(<App />);
-  const loginButton = screen.getByText(/login/i);
-  expect(loginButton).toBeInTheDocument();
-});
